55b278218309644907595495649421379023254c
lefty313 <lewy313@gmail.com>
2012-09-20 22:39:19 +0200

    Added moving windows sample

---
+++ samples/moving_windows.rb
@@ -0,0 +1,23 @@
+$:.unshift(File.expand_path(File.join(File.dirname(__FILE__), "../lib/")))
+require 'pry'
+require 'windows'
+
+class Window < Struct.new(:command)
+  include Windows::Manager
+end
+
+terminal = Window.new('gnome-terminal')
+editor   = Window.new('subl ../')
+
+windows = []
+windows << terminal
+windows << editor
+
+begin
+  windows.each(&:create)
+  editor.move(0,0, '100%', '100%')
+  terminal.move('50%','50%', '50%','50%')
+  sleep 5
+ensure
+  windows.each(&:close)
+end
